d7ea7cc860138350b41a6fc6b6058cbe7c8748a6
naru-77 <85442776+naru-77@users.noreply.github.com>
2021-07-14 17:49:16 +0900

    Update station13.js

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,8 +1,8 @@
-const arrowFunction =()=> {
+const arrowFunction = () =>　{
     return "Arrow"
 }
 
 function getData() {
-    // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
     return arrowFunction()
-}
+};
+
