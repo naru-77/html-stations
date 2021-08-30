a441f75977bbfdedb5f36bf4354812adbe7ab0cf
naru-77 <85442776+naru-77@users.noreply.github.com>
2021-08-30 14:54:01 +0900

    Update station14.js

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,7 +6,7 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+     return test.map(x=>buildFullName(x))
 }
 
 function buildFullName(data) {
@@ -14,9 +14,9 @@ function buildFullName(data) {
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
     const full_name = data.family_name + ' ' + data.first_name;
-    data["full_name"] = full_name;
-    return data
+  return {...data,full_name};
 }
+console.log(getData());
 
 // 出力が想定される答え
 // const test = [
