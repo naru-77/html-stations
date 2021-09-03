ffd46166d7e699dbd865fb77d97e1c7b97e373a0
naru-77 <85442776+naru-77@users.noreply.github.com>
2021-09-03 10:14:09 +0900

    Update station15.js

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  promise = new Promise ((resolve) => {
+    setTimeout(() => {
+      const result = userList.map((v) => ({
+        ...v,
+        full_name: `${v.family_name} ${v.first_name}`,
+      }));
+      resolve(result);
+    },3000);
+  });
+    return promise;
 }
 
